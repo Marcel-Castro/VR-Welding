06c1eb56e12d9c42614c91bc5f1657c9abdf3fa7
Marcel <castrolecram@gmail.com>
2020-02-28 19:07:17 -0500

    Initialized project

---
+++ VR Welding/Assets/Scripts/Rope.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    public float RigidbodyMass = 1f;
+    public float ColliderRadius = 0.1f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
