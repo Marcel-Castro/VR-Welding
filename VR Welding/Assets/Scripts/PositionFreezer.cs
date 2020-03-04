3379d6b7b8161087f84ab6ec58d5f7c91391c398
dpere361 <46545283+dpere361@users.noreply.github.com>
2020-03-04 01:13:24 -0500

    Position Freezer script created
    Oxygen tank valve interactable

---
+++ VR Welding/Assets/Scripts/PositionFreezer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionFreezer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.GetComponent<Transform>().transform.localPosition = new Vector3(0,0,0);
+    }
+}
