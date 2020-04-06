d878e2c9b06c62a917a70b0d9970a75b2a6a9dbe
dpere361 <46545283+dpere361@users.noreply.github.com>
2020-04-06 13:32:01 -0400

    Started Working on lesson UI

---
+++ VR Welding/Assets/Scripts/LessonController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lesson_Controller : MonoBehaviour
+{
+    public GameObject oxyTank;
+    public GameObject aceTank;
+    public GameObject oxyReg;
+    public GameObject aceReg;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if tank is secured
+
+        // if regulators are attached
+
+        // if connected right hoses
+
+        // if connected hoses to the torch
+
+        // if opened oxygen completely
+
+        // if adjusted oxy psi to 40
+
+        // if opened acetalyne 1/2 turn
+
+        // if adjusted ace psi to 10
+
+        // OPTIONAL if adjusted both psi's to torch setting
+    }
+}
