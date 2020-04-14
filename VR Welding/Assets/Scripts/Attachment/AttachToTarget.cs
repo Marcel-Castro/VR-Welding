f2fb92b541fda2062e097296974bbfaf4b8f7992
Marcel <castrolecram@gmail.com>
2020-04-14 04:24:45 -0400

    Additional work performed on torch anchor points
    They still need work

--- VR Welding/Assets/Scripts/Attachment/AttachToTarget.cs
+++ VR Welding/Assets/Scripts/Attachment/AttachToTarget.cs
@@ -11,26 +11,13 @@ public class AttachToTarget : MonoBehaviour
     public Transform container;
     public bool attached;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-    
-    }
-
-
     void OnTriggerEnter(Collider other) {
         if (other == targetTrigger) {
             // Set rigidbody of parent object to kinematic
             toAnchor.gameObject.GetComponent<Rigidbody>().isKinematic = true;
             // Disable grab script on parent object
             toAnchor.gameObject.GetComponent<ManipulationHandler>().enabled = false;
+
             // Make current object the parent of what was the parent object
             /*
                 Note: Unity does not allow you to directly set a child of an object as that object's parent
@@ -38,6 +25,7 @@ public class AttachToTarget : MonoBehaviour
             */
             transform.SetParent(container);
             toAnchor.SetParent(transform);
+
             // Set transform of current object to match the transform of anchor point (both are empties)
             transform.position = targetTrigger.transform.position;
             transform.rotation = targetTrigger.transform.rotation;
